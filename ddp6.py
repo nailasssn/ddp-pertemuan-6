cf45d282737156ced24fd71d64aa87fbde6791c7
nailasssn <nailahhusnatuada@gmail.com>
2024-11-01 05:38:09 +0700

    ddp6nailahhusna.py

--- ddp6.py
+++ ddp6.py
@@ -1,1 +1,21 @@
-{"nbformat":4,"nbformat_minor":0,"metadata":{"colab":{"provenance":[],"authorship_tag":"ABX9TyNrO/GWUcTOeYkj9SGk6g8Q"},"kernelspec":{"name":"python3","display_name":"Python 3"},"language_info":{"name":"python"}},"cells":[{"cell_type":"code","execution_count":null,"metadata":{"colab":{"base_uri":"https://localhost:8080/"},"id":"O-AE7pedTgb6","executionInfo":{"status":"ok","timestamp":1730257530284,"user_tz":-420,"elapsed":401,"user":{"displayName":"nailah husna tuada","userId":"12536048064868569206"}},"outputId":"11d195e3-6c4a-44fc-8c6a-7e536e69e5b9"},"outputs":[{"output_type":"stream","name":"stdout","text":["951\n","651\n","69\n","319\n","601\n","485\n","507\n","725\n","547\n","615\n","83\n","165\n","141\n","501\n","263\n","617\n","865\n","575\n","219\n","105\n","941\n","47\n","907\n","375\n","823\n","597\n","615\n","953\n","345\n","399\n","219\n","237\n","949\n","687\n","217\n","815\n","67\n","767\n","553\n"]}],"source":["# tugas 1\n","\n","numbers = [\n"," 951, 402, 984, 651, 360, 69, 408, 319, 601, 485, 980, 507, 725,\n","547, 544, 615, 83, 165, 141, 501, 263, 617, 865, 575, 219, 390,\n","984, 592, 236, 105, 942, 941, 386, 462, 47, 418, 907, 344, 236,\n","375, 823, 566, 597, 978, 328, 615, 953, 345, 399, 162, 758, 219,\n","918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217, 815,\n","67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 843, 831, 445,\n","742, 717, 958, 609, 842, 451, 688, 753, 854, 685, 93, 857, 440,\n","380, 126, 721, 328, 753, 470, 743, 527\n","]\n","\n","# print(len(numbers))\n","i = 0\n","while i < len(numbers):\n","    if numbers[i] % 2 == 1:\n","      print(numbers[i])\n","    if numbers[i] == 553:\n","      break\n","    i += 1"]},{"cell_type":"code","source":["jumlah = 0\n","string = \"\"\n","for i in range(1, 20, 2):\n","  jumlah += i\n","  string += f\"{i}\"\n","  if i == 19 :\n","    string += \"+\"\n","  else :\n","    string += \"+\"\n","print(f\"{string} {jumlah}\")"],"metadata":{"colab":{"base_uri":"https://localhost:8080/"},"id":"uZYb0IJ_YkKn","executionInfo":{"status":"ok","timestamp":1730258653385,"user_tz":-420,"elapsed":421,"user":{"displayName":"nailah husna tuada","userId":"12536048064868569206"}},"outputId":"d5ef7da1-d5b2-4f56-e944-516640ddfccc"},"execution_count":null,"outputs":[{"output_type":"stream","name":"stdout","text":["1+3+5+7+9+11+13+15+17+19+ 100\n"]}]},{"cell_type":"code","source":["# tugas 3\n","\n","b = int(input(\"masukan jumlah baris\"))\n","for i in range(1, b+1):\n","  print(\"*\"*i)"],"metadata":{"colab":{"base_uri":"https://localhost:8080/"},"id":"0KYN03cZeBk_","executionInfo":{"status":"ok","timestamp":1730259345240,"user_tz":-420,"elapsed":7114,"user":{"displayName":"nailah husna tuada","userId":"12536048064868569206"}},"outputId":"23246213-b523-4cdc-8da5-844af123c891"},"execution_count":38,"outputs":[{"output_type":"stream","name":"stdout","text":["masukan jumlah baris5\n","*\n","**\n","***\n","****\n","*****\n"]}]}]}
+# tugas 1
+
+numbers = [
+951, 402, 984, 651, 360, 69, 408, 319, 601, 485, 980, 507, 725,
+547, 544, 615, 83, 165, 141, 501, 263, 617, 865, 575, 219, 390,
+984, 592, 236, 105, 942, 941, 386, 462, 47, 418, 907, 344, 236,
+375, 823, 566, 597, 978, 328, 615, 953, 345, 399, 162, 758, 219,
+918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217, 815,
+67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 843, 831, 445,
+742, 717, 958, 609, 842, 451, 688, 753, 854, 685, 93, 857, 440,
+380, 126, 721, 328, 753, 470, 743, 527
+]
+
+# print(len(numbers))
+i = 0
+while i < len(numbers):
+    if numbers[i] % 2 == 1:
+      print(numbers[i])
+    if numbers[i] == 553:
+      break
+    i += 1
